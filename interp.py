cbf1edd01bc6b4950e284e835c47d727fcad4906
Joel Garcia-Cervantes <garciac2@ada.cs.pdx.edu>
2025-04-23 04:07:59 -0700

    interp.py creation

---
+++ interp.py
@@ -0,0 +1,62 @@
+'''
+Joel Garcia-Cervantes 
+CS358 Principles of Programming Languages
+Spring 2025
+
+This file contains AST node definitions for expressions, an eval method for interpreting
+expressions to values, and a run method for showing values to the use. It is part of Milestone 1 for the
+term project.
+
+'''
+from dataclasses import dataclass
+
+type Literal = int
+
+type Expr = Or | And | Not | Lit
+
+
+
+type Expr = Lit | Add | Sub | Mul | Div | Neg 
+
+@dataclass
+class Lit():
+    value: Literal
+    def __str__(self) -> str:
+        return f"{self.value}"
+
+@dataclass
+class Add():
+    left: Expr
+    right: Expr
+    def __str__(self) -> str:
+        return f"({self.left} + {self.right})"
+
+@dataclass
+class Sub():
+    left: Expr
+    right: Expr
+    def __str__(self) -> str:
+        return f"({self.left} - {self.right})"
+
+@dataclass
+class Mul():
+    left: Expr
+    right: Expr
+    def __str__(self) -> str:
+        return f"({self.left} * {self.right})"
+
+@dataclass
+class Div():
+    left: Expr
+    right: Expr
+    def __str__(self) -> str:
+        return f"({self.left} / {self.right})"
+    
+@dataclass
+class Neg():
+    subexpr: Expr
+    def __str__(self) -> str:
+        return f"(- {self.subexpr})"
+    
+
+
