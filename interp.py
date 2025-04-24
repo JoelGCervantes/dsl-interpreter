018dd91f7a4ce93c74123d3f1a573eb275fd5864
Joel Garcia-Cervantes <garciac2@ada.cs.pdx.edu>
2025-04-23 17:07:07 -0700

    update4

--- interp.py
+++ interp.py
@@ -27,7 +27,7 @@ type Expr = Or | And | Not | boolLit \
             | intLit | Add | Sub | Mul | Div | Neg \
             |  Let | Name \
             | Eq | Lt | If \
-            | strLit | Concat | Length
+            | Concat | Length
 
 
 @dataclass
@@ -134,11 +134,6 @@ class If: # if expression
     def __str__(self) -> str:
         return f"(if {self.cond} then {self.then_branch} else {self.else_branch})"
 
-@dataclass
-class strLit():
-    value: str
-    def __str__(self) -> str:
-        return f"{self.value}"
 
 @dataclass
 class Concat():
@@ -192,32 +187,120 @@ def eval(e: Expr) -> Value:
 
 def evalInEnv(env: Env[Value], e:Expr) -> Value:
     match e:
+        # boolean expression forms
         case Or(l,r):
-            return evalInEnv(env,l) or evalInEnv(env,r)
+            lv, rv = evalInEnv(env,l), evalInEnv(env,r)
+            if isinstance(lv, bool) and isinstance(rv, bool):
+                return lv or rv
+            else:
+                raise EvalError(f"or operator requires two boolean operands, but got {l} and {r}")
+        case And(l,r):
+            lv, rv = evalInEnv(env,l), evalInEnv(env,r)
+            if isinstance(lv, bool) and isinstance(rv, bool):
+                return lv and rv
+            else:
+                raise EvalError(f"and operator requires two boolean operands, but got {l} and {r}")
+        case Not(s):
+            sv = evalInEnv(env,s)
+            if isinstance(sv, bool):
+                return not sv
+            else:
+                raise EvalError(f"not operator requires a boolean operand, but got {s}")
+            
+        # arithmetic(int) expression forms
         case Add(l,r):
-            return evalInEnv(env,l) + evalInEnv(env,r)
+            lv, rv = evalInEnv(env,l), evalInEnv(env,r)
+            if isinstance(lv, int) and isinstance(rv, int):
+                return lv + rv
+            elif isinstance(lv, Str) and isinstance(rv, Str):
+                return Str(lv.value + rv.value)
+            else:
+                raise EvalError(f"add operator requires two integer or string operands, but got {l} and {r}")
         case Sub(l,r):
-            return evalInEnv(env,l) - evalInEnv(env,r)
+            lv, rv = evalInEnv(env,l), evalInEnv(env,r)
+            if isinstance(lv, int) and isinstance(rv, int):
+                return lv - rv
+            else:
+                raise EvalError(f"sub operator requires two integer operands, but got {l} and {r}")
         case Mul(l,r):
-            lv = evalInEnv(env,l)
-            rv = evalInEnv(env,r)
-            return lv * rv
+            lv, rv = evalInEnv(env,l), evalInEnv(env,r)
+            if isinstance(lv, int) and isinstance(rv, int):
+                return lv * rv
+            else:
+                raise EvalError(f"mul operator requires two integer operands, but got {l} and {r}")
         case Div(l,r):
             lv = evalInEnv(env,l)
             rv = evalInEnv(env,r)
             if rv == 0:
                 raise EvalError("division by zero")
-            return lv // rv
+            if isinstance(lv, int) and isinstance(rv, int):
+                return lv // rv
         case Neg(s):
-            return - (evalInEnv(env,s))
-        case Lit(i):
-            return i
+            sv = evalInEnv(env,s)
+            if isinstance(sv, int):
+                return -sv
+            else:
+                raise EvalError(f"neg operator requires an integer operand, but got {s}")
+
+        # binding/variable forms 
         case Name(n):
             v = lookupEnv(n, env)
             if v is None:
                 raise EvalError(f"unbound name {n}")
             return v
-        case Let(n,d,b):
-            v = evalInEnv(env, d)
+        case Let(n,defexpr,bodyexpr):
+            v = evalInEnv(env, defexpr)
             newEnv = extendEnv(n, v, env)
-            return evalInEnv(newEnv, b)
+            return evalInEnv(newEnv, bodyexpr)
+
+        # comparison forms
+        case Eq(l,r):
+            lv, rv = evalInEnv(env,l), evalInEnv(env,r)
+            if isinstance(lv, bool) and isinstance(rv, bool):
+                return lv == rv
+            elif isinstance(lv, int) and isinstance(rv, int):
+                return lv == rv
+            elif isinstance(lv, Str) and isinstance(rv, Str):
+                return lv == rv
+            else:
+                raise EvalError(f"eq operator requires two operands of the same type, but got {l} and {r}")
+        case Lt(l,r):
+            lv, rv = evalInEnv(env,l), evalInEnv(env,r)
+            if isinstance(lv, int) and isinstance(rv, int):
+                return lv < rv
+            else:
+                raise EvalError(f"lt operator requires two integer operands, but got {l} and {r}")
+        
+        # conditional forms
+        case If(cond, then_branch, else_branch):
+            condv = evalInEnv(env, cond)
+            if isinstance(condv, bool):
+                if condv:
+                    return evalInEnv(env, then_branch)
+                else:
+                    return evalInEnv(env, else_branch)
+            else:
+                raise EvalError(f"if operator requires a boolean condition, but got {cond}")
+        # literals
+        case boolLit(v):
+            return v
+        case intLit(v):
+            return v
+        case Str(v):
+            return Str(v)
+        
+        # string operator forms 
+        case Concat(l,r):
+            lv, rv = evalInEnv(env,l), evalInEnv(env,r)
+            if isinstance(lv, Str) and isinstance(rv, Str):
+                return Str(lv.value + rv.value)
+            else:
+                raise EvalError(f"concat operator requires two string operands, but got {l} and {r}")
+        case Length(s):
+            sv = evalInEnv(env,s)
+            if isinstance(sv, Str):
+                return len(sv.value)
+            else:
+                raise EvalError(f"length operator requires a string operand, but got {s}")
+        case _:
+            raise EvalError(f"unknown expression {e}")
